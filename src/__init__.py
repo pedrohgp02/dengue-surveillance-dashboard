b6cfa3a43c8904f47fdff30d729f637e0c115a7c
Pedro Henrique Goncalves de Paiva <157153490+pedrohgp02@users.noreply.github.com>
2026-08-09 14:38:44 -0400

    Create package public API
    Added public API components for the dengue surveillance package.

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,35 @@
+"""Public API for the dengue surveillance package."""
 
+from src.config import (
+    TARGET_LABEL,
+    TARGET_UF_CODE,
+    TREND_THRESHOLD,
+    UF_OPTIONS,
+)
+
+from src.data import (
+    build_weekly_series,
+    cache_exists,
+    cache_is_fresh,
+    get_available_data_urls,
+    get_cache_info,
+)
+
+from src.evaluation import score_predictions
+
+from src.pipeline import build_monitoring_state
+
+
+__all__ = [
+    "TARGET_LABEL",
+    "TARGET_UF_CODE",
+    "TREND_THRESHOLD",
+    "UF_OPTIONS",
+    "build_weekly_series",
+    "cache_exists",
+    "cache_is_fresh",
+    "get_available_data_urls",
+    "get_cache_info",
+    "score_predictions",
+    "build_monitoring_state",
+]
